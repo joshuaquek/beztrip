4b652b270d2087a7ecbea9bf1c40d490079406af
Joshua Quek <joven2468@gmail.com>
2015-08-29 20:59:56 +0800

    Added access control

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -16,3 +16,19 @@ Router.route('/', {				// Home Page
 Router.configure({				// This defines the layout used for every page,...
 	layoutTemplate: 'main'		// ...where the wrapper page where you can define your HEADER and FOOTER
 });
+
+
+var requireLogin = function() {
+	if (! Meteor.user()) {
+		// If user is not logged in render login
+		this.render('login');
+	} else {
+		//if user is logged in render whatever route was requested
+		this.next();
+	}
+}
+
+// Before any routing run the requireLogin function.
+// Except in the case of "landingpage".
+// Note that you can add more pages in the exceptions if you want. (e.g. About, Faq, contact...)
+Router.onBeforeAction(requireLogin, {except: ['landingpage']});
