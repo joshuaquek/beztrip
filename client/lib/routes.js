bfd6ca7188953f59a4a1bdeed806c0e7861ee913
rswy <ranon.sim.2013@sis.smu.edu.sg>
2015-08-30 07:33:18 +0800

    All except homepage, nav bar, addtodocuments

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -9,8 +9,30 @@ Router.route('/', {				// Home Page
 	template: 'home'
 });
 
+Router.route('/itinerary', {				// Home Page
+	name: 'itinerary',
+	template: 'itinerary'
+});
 
+Router.route('/suggestions', {				// suggestions
+	name: 'suggestions',
+	template: 'suggestions'
+});
 
+Router.route('/managetrips', {				// Manage Trips
+	name: 'managetrips',
+	template: 'managetrips'
+});
+
+Router.route('/finances', {				// Finances
+	name: 'finances',
+	template: 'finances'
+});
+
+Router.route('/documents', {				// Finances
+	name: 'documents',
+	template: 'documents'
+});
 //---------------------------------- ROUTER CONFIG ------------------------------
 
 Router.configure({				// This defines the layout used for every page,...
