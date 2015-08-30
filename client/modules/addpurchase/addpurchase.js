06f9adc830783f0c6bd42865e80f7ceede7b45c4
rswy <ranon.sim.2013@sis.smu.edu.sg>
2015-08-30 09:22:40 +0800

    BASD

--- client/modules/addpurchase/addpurchase.js
+++ client/modules/addpurchase/addpurchase.js
@@ -5,7 +5,8 @@ purchases = new Mongo.Collection("purchases");
 Template.addpurchase.events({
 	'click .addpurchase': function(e){
 		e.preventDefault();
-		Session.set('purchase', "drink");
+		Session.set("purchase", "drink");
+		console.log('purccchase: ', "AGJASJHDKASH");
 		// Purchases.insert({type: "Drink", amount: "$25.00", createdAt: new Date() });
 		Router.go('/finances');
 	}
